1407421bc1c50d794fd521634358193a44971fba
jghee <ghj171937@gmail.com>
2022-07-04 20:25:00 +0900

    Create Tenants Form

---
+++ dcos/dcim/forms/models.py
@@ -0,0 +1,13 @@
+from django import forms
+
+from dcim.models import *
+
+class TenantGroupForm(forms.ModelForm):
+    class Meta:
+        model = TenantGroup
+        fields = ['name', 'slug', ]
+
+class TenantForm(forms.ModelForm):
+    class Meta:
+        model = Tenant
+        fields = ['name', 'slug', 'group', ]
