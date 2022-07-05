c2e089c1691c273f38a103ab95d6babbd4bdf0e9
jghee <ghj171937@gmail.com>
2022-07-05 17:30:20 +0900

    Add Admin_Tenants

--- dcos/dcim/admin.py
+++ dcos/dcim/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import *
+
+
+class TenantGroupAdmin(admin.ModelAdmin):
+    fields = ['name', 'slug']
+
+
+class TenantAdmin(admin.ModelAdmin):
+    fields = ['pk', 'name', 'slug', 'group']
+
+
+admin.site.register(TenantGroup, TenantGroupAdmin)
+admin.site.register(Tenant, TenantAdmin)
