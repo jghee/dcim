cae0ca041caa9231dce8cfdec8ae664336851349
jghee <ghj171937@gmail.com>
2022-07-06 16:55:35 +0900

    Tenants URL conf

--- dcos/dcim/forms/models.py
+++ dcos/dcim/forms/models.py
@@ -2,10 +2,12 @@ from django import forms
 
 from dcim.models import *
 
+
 class TenantGroupForm(forms.ModelForm):
     class Meta:
         model = TenantGroup
         fields = ['name', 'slug', ]
+
 
 class TenantForm(forms.ModelForm):
     class Meta:
