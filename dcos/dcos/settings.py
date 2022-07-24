defb9b3b5796df6eb31a08f3a72093b0c5251ba8
jghee <ghj171937@gmail.com>
2022-07-24 17:06:38 +0900

    Add a apps 'django-debug-toolbar'

--- dcos/dcos/settings.py
+++ dcos/dcos/settings.py
@@ -39,9 +39,11 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'dcim.apps.DcimConfig',
     'django_tables2',
+    "debug_toolbar",
 ]
 
 MIDDLEWARE = [
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -125,3 +127,7 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
